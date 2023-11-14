a072f697c5f4ed2c4f6c0353ffdedd5c64c71693
KikkiZ <zhangyozh@foxmail.com>
2023-11-14 21:18:56 +0800

    style: 优化代码格式

--- models/common.py
+++ models/common.py
@@ -1,39 +1,47 @@
+from typing import Dict
+
 import torch
 import torch.nn as nn
 import numpy as np
 from .downsampler import Downsampler
 
+
 def add_module(self, module):
     self.add_module(str(len(self) + 1), module)
-    
+
+
 torch.nn.Module.add = add_module
 
+
 class Concat(nn.Module):
-    def __init__(self, dim, *args):
+    def __init__(self, dim, modules: Dict[str, nn.Module]):
         super(Concat, self).__init__()
         self.dim = dim
 
-        for idx, module in enumerate(args):
-            self.add_module(str(idx), module)
+        for key, value in modules.items():
+            # print(type(module))
+            self.add_module(key, value)
 
-    def forward(self, input):
+    def forward(self, input_data):
         inputs = []
+        # _modules.values(): contains all operations in the current layer (layer)
         for module in self._modules.values():
-            inputs.append(module(input))
+            inputs.append(module(input_data))
 
-        inputs_shapes2 = [x.shape[2] for x in inputs]
-        inputs_shapes3 = [x.shape[3] for x in inputs]        
+        inputs_shapes_2 = [x.shape[2] for x in inputs]
+        inputs_shapes_3 = [x.shape[3] for x in inputs]
 
-        if np.all(np.array(inputs_shapes2) == min(inputs_shapes2)) and np.all(np.array(inputs_shapes3) == min(inputs_shapes3)):
+        if np.all(np.array(inputs_shapes_2) == min(inputs_shapes_2)) and np.all(
+                np.array(inputs_shapes_3) == min(inputs_shapes_3)):
             inputs_ = inputs
         else:
-            target_shape2 = min(inputs_shapes2)
-            target_shape3 = min(inputs_shapes3)
+            target_shape2 = min(inputs_shapes_2)
+            target_shape3 = min(inputs_shapes_3)
 
             inputs_ = []
-            for inp in inputs: 
-                diff2 = (inp.size(2) - target_shape2) // 2 
-                diff3 = (inp.size(3) - target_shape3) // 2 
+            for inp in inputs:
+                diff2 = (inp.size(2) - target_shape2) // 2
+                diff3 = (inp.size(3) - target_shape3) // 2
                 inputs_.append(inp[:, :, diff2: diff2 + target_shape2, diff3:diff3 + target_shape3])
 
         return torch.cat(inputs_, dim=self.dim)
@@ -47,12 +55,12 @@ class GenNoise(nn.Module):
         super(GenNoise, self).__init__()
         self.dim2 = dim2
 
-    def forward(self, input):
-        a = list(input.size())
+    def forward(self, inputs):
+        a = list(inputs.size())
         a[1] = self.dim2
         # print (input.data.type())
 
-        b = torch.zeros(a).type_as(input.data)
+        b = torch.zeros(a).type_as(inputs.data)
         b.normal_()
 
         x = torch.autograd.Variable(b)
@@ -70,10 +78,10 @@ class Swish(nn.Module):
         return x * self.s(x)
 
 
-def act(act_fun = 'LeakyReLU'):
-    '''
+def act(act_fun='LeakyReLU'):
+    """
         Either string defining an activation function or module (e.g. nn.ReLU)
-    '''
+    """
     if isinstance(act_fun, str):
         if act_fun == 'LeakyReLU':
             return nn.LeakyReLU(0.2, inplace=True)
@@ -86,14 +94,28 @@ def act(act_fun = 'LeakyReLU'):
         else:
             assert False
     else:
-        return act_fun()
+        return act_fun
 
 
 def bn(num_features):
+    """Batch normalized layers
+
+    Argument:
+        num_features: the number of channels of the eigenvector
+    """
     return nn.BatchNorm2d(num_features)
 
 
 def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+    """Convolution layer
+    Returns a convolution layer containing downsampling and reflection padding.
+
+    Argument:
+        in_f: the number of input channels
+        out_f: the number of output channels
+        pad: setting padding mode
+        downsample_mode: setting downsample mode
+    """
     downsampler = None
     if stride != 1 and downsample_mode != 'stride':
 
@@ -101,21 +123,21 @@ def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_m
             downsampler = nn.AvgPool2d(stride, stride)
         elif downsample_mode == 'max':
             downsampler = nn.MaxPool2d(stride, stride)
-        elif downsample_mode  in ['lanczos2', 'lanczos3']:
-            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5, preserve_size=True)
+        elif downsample_mode in ['lanczos2', 'lanczos3']:
+            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5,
+                                      preserve_size=True)
         else:
             assert False
 
         stride = 1
 
-    padder = None
+    padding = None
     to_pad = int((kernel_size - 1) / 2)
     if pad == 'reflection':
-        padder = nn.ReflectionPad2d(to_pad)
+        padding = nn.ReflectionPad2d(to_pad)
         to_pad = 0
-  
+
     convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
 
-
-    layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+    layers = filter(lambda x: x is not None, [padding, convolver, downsampler])
     return nn.Sequential(*layers)
--- utils/denoising_utils.py
+++ utils/denoising_utils.py
@@ -1,4 +1,7 @@
 import os
+
+import numpy as np
+
 from .common_utils import *
 
 
